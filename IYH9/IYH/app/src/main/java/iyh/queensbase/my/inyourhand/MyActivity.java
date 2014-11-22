9c0f4df9e238d4a250e7a26b90a5c87a605e2668
SabrinaCullen <sab_123ong@hotmail.com>
2014-11-22 22:09:05 +0800

    1st

---
+++ IYH9/IYH/app/src/main/java/iyh/queensbase/my/inyourhand/MyActivity.java
@@ -0,0 +1,46 @@
+package iyh.queensbase.my.inyourhand;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void next(View v){
+        Log.i("clicks","you clicked next");
+        Intent i = new Intent(MyActivity.this, SignUp.class);
+        startActivity(i);
+
+    }
+}
